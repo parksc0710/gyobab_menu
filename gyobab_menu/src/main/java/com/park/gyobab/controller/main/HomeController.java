ed8136f6fdf495964b4faed24d6d7c0a24d28ad4
parks <parks@DESKTOP-6D57EQS>
2021-05-10 00:37:34 +0900

    메인 페이지 온누리교회 바코드 출력

--- gyobab_menu/src/main/java/com/park/gyobab/controller/main/HomeController.java
+++ gyobab_menu/src/main/java/com/park/gyobab/controller/main/HomeController.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.FileUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.park.gyobab.domain.MemberVO;
 
 @Controller
 public class HomeController{
@@ -32,13 +35,6 @@ public class HomeController{
 	
 	@RequestMapping(value = "/main", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "main";
 	}
--- gyobab_menu/src/main/webapp/WEB-INF/views/main.jsp
+++ gyobab_menu/src/main/webapp/WEB-INF/views/main.jsp
@@ -2,5 +2,25 @@
 <%@ taglib uri="http://www.springframework.org/security/tags" prefix="security" %>
 
 <div class="content">
-	안녕? 여기는 메인페이지야
+	<img src="https://www.gyobab.shop:8883/images/hello.jpg" style="width:100%;max-width:639px;"/><br>
+	이미지가 보이지 않으면 아래 버튼을 눌러서 인증서 허용을 해주세요.<br>
+	<a href="https://www.gyobab.shop:8883/images/hello.jpg" target="_blank" class="btn btn-primary btn-sm btn-block" style="width:150px;">
+		<i class="fas fa-search"></i> 인증서 허용하기
+	</a>
+	<br><br>
+	<security:authorize ifAnyGranted="ROLE_OPERATOR, ROLE_ADMIN, ROLE_USER">
+	<security:authentication property="principal.member_pass" var="memberPass"/>
+		<div class="row">
+		    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">
+		        <div class="card mb-3">
+		            <div class="card-header">
+		                <h3>내 온누리교회 바코드</h3>
+		            </div>
+		            <div class="card-body">
+		            	<img src="${memberPass }" style="display: block; margin: 0px auto;"/>
+		            </div>
+		        </div>
+		    </div>
+		</div>
+	</security:authorize>
 </div>
